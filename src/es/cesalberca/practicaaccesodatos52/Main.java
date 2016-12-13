bc213d8c4177db0db40629de82c2cf04f52be285
cesalberca <cesalberca@gmail.com>
2016-12-13 20:12:24 +0100

    Conexion a base de datos

--- src/es/cesalberca/practicaaccesodatos52/Main.java
+++ src/es/cesalberca/practicaaccesodatos52/Main.java
@@ -5,10 +5,40 @@ import es.cesalberca.practicaaccesodatos52.modelo.ConectorBD;
 import es.cesalberca.practicaaccesodatos52.modelo.Modelo;
 import es.cesalberca.practicaaccesodatos52.vista.Vista;
 
+import java.sql.SQLException;
+
 public class Main {
 
+    public final static String nombreDB =  "XE";
+    public final static String usuario = "HR";
+    public final static String contrasena = "HR";
+
     public static void main(String[] args) {
-        ConectorBD conectorBD = new ConectorBD();
+
+        ConectorBD conectorBD = null;
+
+        try {
+            conectorBD = new ConectorBD(nombreDB, usuario, contrasena);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (conectorBD != null) {
+                conectorBD.inicarConexion();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (conectorBD != null) {
+                conectorBD.test();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         Vista vista = new Vista();
         Modelo modelo = new Modelo();
         Controlador controlador = new Controlador(vista, modelo);
--- src/es/cesalberca/practicaaccesodatos52/modelo/ConectorBD.java
+++ src/es/cesalberca/practicaaccesodatos52/modelo/ConectorBD.java
@@ -1,7 +1,44 @@
 package es.cesalberca.practicaaccesodatos52.modelo;
 
+import java.sql.*;
+
 /**
  * Created by César
  */
 public class ConectorBD {
+    private Connection conexion;
+    private String nombreDB;
+    private String usuario;
+    private String contrasena;
+
+    private static final String DB_CONNECTION = "jdbc:oracle:thin:@//localhost:1521/XE";
+
+    public ConectorBD(String nombreDB, String usuario, String contrasena) throws ClassNotFoundException {
+        this.nombreDB = nombreDB;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        Class.forName("oracle.jdbc.OracleDriver");
+    }
+
+    public void inicarConexion() throws SQLException {
+        String url = DB_CONNECTION;
+        conexion = DriverManager.getConnection(url, usuario, contrasena);
+    }
+
+    public void cerrarConexion() throws SQLException {
+        if (!conexion.isClosed())
+            conexion.close();
+    }
+
+    public void test() throws SQLException {
+        String selectSQL = "SELECT * FROM EMPLOYEES WHERE USER_ID = ?";
+        PreparedStatement preparedStatement = conexion.prepareStatement(selectSQL);
+        preparedStatement.setInt(1, 100);
+        ResultSet rs = preparedStatement.executeQuery(selectSQL );
+        while (rs.next()) {
+            String userid = rs.getString("FIRST_NAME");
+            String username = rs.getString("LAST_NAME");
+        }
+    }
+
 }
